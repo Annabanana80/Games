66a47502d29c0b530c24f23c306036e82268cdce
Anna Sanderson <anna.sanderson@csuglobal.edu>
2016-06-27 15:14:28 -0400

    Squares now clickable

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 window.onload = function() {
-	console.log(app.js);
+	console.log('app.js');
 
-	var squares = document.getElementByClassName('square');
+	var squares = document.getElementsByClassName('square');
 		for(var i=0; i<squares.length; i++){
-			squares[i].addEventListener('click', play);
+			squares[i].addEventListener('click', function(event){
+				console.log(this);
+			});
 	}
 }
 
 function play (){
-	
+
 }
