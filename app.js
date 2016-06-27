a3cc0539bb03d3ab00467b92facf4b08c27bf751
Anna Sanderson <anna.sanderson@csuglobal.edu>
2016-06-27 19:17:38 -0400

    Got x and o to display. Used previous hw as a guide and of course cited it. Need to work out bug that first tile shows undefined

--- app.js
+++ app.js
@@ -3,21 +3,32 @@ window.onload = function() {
 
 	var squares = document.getElementsByClassName('square');
 		for(var i=0; i<squares.length; i++){
-			squares[i].addEventListener('click', function(event){
-				console.log(this);
-			});
+			squares[i].addEventListener('click', playMe);
+		}
 	}
+
+
+function start (){
+	document.start = "X"  //used from previous homework. This ensures that "X" goes first.
 }
 
-// var x = [];
-// var o = [];
-
-// var winners = 
-
 //display an X or O inside the board
-// function playMe (){
-// 	if(this.)
-// }
+ function playMe (){
+ 	var squares = document.getElementsByClassName('square');
+	if(squares.innerText =" "){
+		this.innerText = document.start;
+		turn(); //calls the function so the squares display the X or O
+	}else{
+		alert ("The square is taken")
+	}
+}
+function turn (){				//this switches turns. Used from previous homework.
+	if (document.start==="X"){
+		document.start ="O";
+	}else{
+		document.start="X"
+	}
+}
 
 //make sure nothing happens if already clicked
 //reset the game--connect this to reset button. 
