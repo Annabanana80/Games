4aedaf24a550569e2fbb2ad6942f59ea0c35b2da
Anna Sanderson <anna.sanderson@csuglobal.edu>
2016-06-27 20:30:12 -0400

    fixed bug

--- app.js
+++ app.js
@@ -1,34 +1,52 @@
 window.onload = function() {
 	console.log('app.js');
 
+	
+	}
+
+
+var currentPlayer = '';
+
+function start () {
+	currentPlayer  = 'X';
 	var squares = document.getElementsByClassName('square');
 		for(var i=0; i<squares.length; i++){
-			squares[i].addEventListener('click', playMe);
-		}
-	}
-
-
-function start (){
-	document.start = "X"  //used from previous homework. This ensures that "X" goes first.
+			squares[i].addEventListener('click', function(){
+				playMe(this);
+		});
+ //used from previous homework. This ensures that "X" goes first.
 }
+}
+
+start();
 
 //display an X or O inside the board
- function playMe (){
- 	var squares = document.getElementsByClassName('square');
-	if(squares.innerText =" "){
-		this.innerText = document.start;
-		turn(); //calls the function so the squares display the X or O
-	}else{
-		alert ("The square is taken")
-	}
-}
-function turn (){				//this switches turns. Used from previous homework.
-	if (document.start==="X"){
-		document.start ="O";
-	}else{
-		document.start="X"
-	}
-}
+ function playMe (square){
+ 	console.log(square)
+ 		if(square.innerHTML = " "){
+			square.innerHTML = currentPlayer;
+			if (currentPlayer === 'X'){
+				currentPlayer = 'O'
+			} else  {
+				currentPlayer = 'X'
+			}
+			}else{
+			alert ("The square is taken")
+		}	
+ 	};	
+
+ // function turn (){				//this switches turns. Used from previous homework.
+	//  if (document.start === "X"){
+	//  	document.start ="O";
+ // 	}else{
+ // 		document.start="X";
+	// }
+ // }
 
 //make sure nothing happens if already clicked
 //reset the game--connect this to reset button. 
+
+// function clear (){
+// 	var clearMe=document.getElementById('button');
+
+// }
