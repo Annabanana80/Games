a63b1495e4b3dac126da602d3d971b5176246a8b
Anna Sanderson <anna.sanderson@csuglobal.edu>
2016-06-28 15:38:04 -0400

    Got the rows to no longer click twice but for some reason, the board shrinks.

--- app.js
+++ app.js
@@ -12,29 +12,54 @@ function start () {
 	var squares = document.getElementsByClassName('square');	//grabbing the squares
 		for(var i=0; i<squares.length; i++){					
 			squares[i].addEventListener('click', function(){	//making all of the squares clickable
-				this.innerHTML = '';
-				playMe(this);									//calling the playMe function to make the X's and O's display
+				//this.innerHTML = '';
+				playMe(this);	//calling the playMe function to make the X's and O's display
+											
 		});
 	}
 }
+
+//reset the game--connect this to reset button.Still a work in progress 
+
+//  function clear (){
+// 	var clearMe=document.getElementById('button');
+// 	for(var i=0; i<squares.length;i++){
+// 		clearMe.addEventListener('click', function(){
+// 		squares[i].innerHTML = '';			this should somehow reset the className to nothing and clear the square.
+// 		})
+// 	}
+// }
 
 //var winners = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 start();
 
 //display an X or O inside the board
- function playMe (square){  
- 	  if(square.innerHTML = " "){			//if there is nothing/square is not played
-		square.innerHTML = currentPlayer;
-											  //then assign it the current player
-		if (currentPlayer === 'X'){		//if the current player played X
-			currentPlayer = 'O';			//the other player is O
-		} else  {
-			currentPlayer = 'X';		//and vice versa
-			}
-		 }else{
+
+  function playMe (square){  
+  	if(!square.className.includes('clicked')){  //This says if the square does not have the class of clicked...Borrowed from TTT solution from what I remember reading. I know it might not be accuate but I wanted to cite it just in case.
+ 	//  	if(square.innerHTML = " "){			//if there is nothing/square is not played
+	 	square.innerHTML = currentPlayer;	  //then assign it the current player 
+	 	if (currentPlayer === 'X'){		//if the current player played X
+	 		currentPlayer = 'O';			//the other player is O
+	 	} else  {
+	 		currentPlayer = 'X';		//and vice versa
+ 		}
+	 	 }else{
 		 	alert ("The square is taken")  //tried to combine the function to stop squares from already being clicked.
-		}	
- 	};	
+	 	}
+	 	square.className += 'clicked' //this establishes a classname for all of the squares played to be classified as clicked.	
+  	};	
+
+//start of winner logic
+  	// function checkForWinners () {
+  	// 	for(var i=0; i<winners.length; i++){
+  	//		if()
+  	// 	}
+  	// }
+
+
+
+
 
  	 // function alreadyPlayed (square){       Attempt at making selected squares not clickable
  	 // 	if (square.innerHTML = " "){
@@ -55,16 +80,7 @@ start();
  // }
 
 //make sure nothing happens if already clicked
-//reset the game--connect this to reset button. 
 
-//  function clear (){
-// 	var clearMe=document.getElementById('button');
-// 	for(var i=0; i<squares.length;i++){
-// 		clearMe.addEventListener('click', function(){
-// 		squares[i].innerHTML = '';
-// 		})
-// 	}
-// }
 
 
 
